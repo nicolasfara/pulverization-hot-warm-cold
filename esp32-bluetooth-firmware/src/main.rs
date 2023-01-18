287c8a3b70cebe76324537488857e06e1d546670
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-01-18 15:34:47 +0100

    refactor: some refactors

--- esp32-bluetooth-firmware/src/main.rs
+++ esp32-bluetooth-firmware/src/main.rs
@@ -1,7 +1,7 @@
-use esp32_nimble::{uuid128, BLEDevice, NimbleProperties};
-use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
+use esp32_nimble::{BLEDevice, NimbleProperties, uuid128};
+use esp_idf_sys as _;
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_sys::link_patches();
@@ -16,10 +16,18 @@ fn main() {
 
     let ble_device = BLEDevice::take();
 
+    // Get BLE server instance
     let server = ble_device.get_server();
+
+    // Register an handler logging the connection
+    server.on_connect(move |_| {
+        ::log::info!("New connection!")
+    });
+
+    // Create a BLE service
     let service = server.create_service(uuid128!("fafafafa-fafa-fafa-fafa-fafafafafafa"));
 
-    // A static characteristic.
+    // A test, static characteristic.
     let static_characteristic = service.lock().create_characteristic(
         uuid128!("d4e0e0d0-1a2b-11e9-ab14-d663bd873d93"),
         NimbleProperties::READ,
@@ -30,8 +38,17 @@ fn main() {
 
     let ble_advertising = ble_device.get_advertising();
     ble_advertising
-        .name("ESP32-GATT-Server")
+        .name("ESP32")
         .add_service_uuid(uuid128!("fafafafa-fafa-fafa-fafa-fafafafafafa"));
 
-    ble_advertising.start().unwrap();
+    match ble_advertising.start() {
+        Ok(_) => {
+            ::log::info!("Bluetooth started...")
+        }
+        Err(err) => {
+            ::log::error!("Failed to start advertising: {}", err.0)
+        }
+    }
+
+    Ok(())
 }
